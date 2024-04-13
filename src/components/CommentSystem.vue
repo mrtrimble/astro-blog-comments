019c55950b2ec401a150dec691d0370a35e1c2c6
Ryan Trimble <mrtrimble2@gmail.com>
2024-04-13 12:23:17 -0400

    adds blog page back

---
+++ src/components/CommentSystem.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { computed, onMounted } from 'vue';
+import CommentForm from './CommentForm.vue';
+import CommentList from './CommentList.vue';
+
+const props = defineProps({
+  slug: String,
+  baseUrl: String,
+});
+const { slug } = props;
+console.log(slug);
+
+</script>
+
+<template>
+  <div class="container">
+    <h2>Post Comments</h2>
+    <CommentForm :slug="slug" />
+    <CommentList :url="`${baseUrl}/api/comments/${props.slug}`" />
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
